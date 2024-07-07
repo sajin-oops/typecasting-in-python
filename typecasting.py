29d55241fca2d7abf1b15099a5e7d322ab5f4af9
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-07 17:11:20 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,18 @@
+'''
+
+otp = 9888
+print(otp)
+print(type(otp))
+print("Your otp is " + str(otp))
+print(type(otp))
+otp = str(otp)
+print(type(otp))
+
+'''
+
+count = "100.3"
+print(type(count))
+print(float(count)+100)
+
+counts = 100
+print(counts+29)
